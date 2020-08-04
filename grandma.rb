3d5080af5e4f75ab1b17219f2755c80083d5133e
Kyler Mejia <kylermejia1@gmail.com>
2020-08-04 02:18:31 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,8 +12,8 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-def speak_to_grandma(phrase = "HUH?! SPEAK UP, SONNY!")
-  puts phrase 
+def speak_to_grandma = "HUH?! SPEAK UP, SONNY!"
+ 
 end
  
 if speak_to_grandma == "I LOVE YOU GRANDMA!"
