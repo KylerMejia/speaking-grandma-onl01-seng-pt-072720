8432788a72d4b989ff8d1b5a7e447badd1413868
Kyler Mejia <kylermejia1@gmail.com>
2020-08-04 02:32:39 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -22,7 +22,7 @@ if speak_to_grandma == "I LOVE YOU GRANDMA!"
   puts "I LOVE YOU TOO PUMPKIN!"
 
 elsif speak_to_grandma == "Hi Nana, how are you?"
-  puts "HUH?! SPEAK UP, SONNY!"
+  puts phrase
 
 elsif speak_to_grandma == "Hi!"
   puts "HUH?! SPEAK UP, SONNY!"
